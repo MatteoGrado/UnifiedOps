9a024dd7348d9a963ea587e0b979949886f41c20
Matteo Grado <matteogrado@macbookpro.speedport.ip>
2026-05-26 17:31:31 +0200

    Basic Setup Finished

---
+++ Consul-Service/src/main/java/de/grado/consulservice/ConsulServiceApplication.java
@@ -0,0 +1,15 @@
+package de.grado.consulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulServiceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConsulServiceApplication.class, args);
+    }
+
+}
---
+++ Consul-Service/src/test/java/de/grado/consulservice/ConsulServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package de.grado.consulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
